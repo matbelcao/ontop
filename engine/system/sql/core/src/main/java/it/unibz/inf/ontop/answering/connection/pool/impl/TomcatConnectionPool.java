6ff4b42ec121cfef2d490a5828c62310064b298e
matbelcao <10503336@polimi.it>
2020-11-15 23:17:23 +0100

    add ksql configuration

--- engine/system/sql/core/src/main/java/it/unibz/inf/ontop/answering/connection/pool/impl/TomcatConnectionPool.java
+++ engine/system/sql/core/src/main/java/it/unibz/inf/ontop/answering/connection/pool/impl/TomcatConnectionPool.java
@@ -40,6 +40,8 @@ public class TomcatConnectionPool implements JDBCConnectionPool {
                 poolProperties.setValidationQuery("select 1 from dual");
             else if (driver.contains("db2"))
                 poolProperties.setValidationQuery("select 1 from sysibm.sysdummy1");
+            else if (driver.contains("ksql"))
+                poolProperties.setValidationQuery("SHOW TABLES");
             else
                 poolProperties.setValidationQuery("select 1");
         }
